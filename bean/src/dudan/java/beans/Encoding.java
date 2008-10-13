b665a5106b6cc488655aae80acefa5842d76f726
lendo.du <lendo.du@06ebe554-1442-0410-b41d-f121fd812e7c>
2008-10-13 14:39:03 +0000

    初始导入。

---
+++ bean/src/dudan/java/beans/Encoding.java
@@ -0,0 +1,25 @@
+package dudan.java.beans;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class Encoding implements Filter {
+	public void destroy() {
+	}
+
+	public void init(FilterConfig config) {
+
+	}
+
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+		request.setCharacterEncoding("UTF-8");
+		chain.doFilter(request, response);
+	}
+}
