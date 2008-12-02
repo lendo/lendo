45a5fa99b66e49d6af5dc3a20eee675a92d21d9b
lendo.du <lendo.du@06ebe554-1442-0410-b41d-f121fd812e7c>
2008-12-02 10:45:10 +0000

    初始导入。

---
+++ trunk/parser/src/com/waioo/parser/BlockBodyTag.java
@@ -0,0 +1,72 @@
+package com.waioo.parser;
+
+import org.htmlparser.tags.CompositeTag;
+
+public class BlockBodyTag extends CompositeTag {
+	/**
+     * The set of names handled by this tag.
+     */
+    private static final String[] mIds = new String[] {"BLOCKBODY"};
+
+    /**
+     * The set of tag names that indicate the end of this tag.
+     */
+    private static final String[] mEnders = new String[] {"BLOCKBODY"};
+
+    /**
+     * The set of end tag names that indicate the end of this tag.
+     */
+    private static final String[] mEndTagEnders = new String[] {"BLOCKBODY"};
+
+    /**
+     * Create a new title tag.
+     */
+    public BlockBodyTag()
+    {
+    }
+
+    /**
+     * Return the set of names handled by this tag.
+     * @return The names to be matched that create tags of this type.
+     */
+    public String[] getIds ()
+    {
+        return (mIds);
+    }
+
+    /**
+     * Return the set of tag names that cause this tag to finish.
+     * @return The names of following tags that stop further scanning.
+     */
+    public String[] getEnders ()
+    {
+        return (mEnders);
+    }
+
+    /**
+     * Return the set of end tag names that cause this tag to finish.
+     * @return The names of following end tags that stop further scanning.
+     */
+    public String[] getEndTagEnders ()
+    {
+        return (mEndTagEnders);
+    }
+
+    /**
+     * Get the title text.
+     * @return The title.
+     */
+    public String getBlockBody()
+    {
+        return toPlainTextString();
+    }
+
+    /**
+     * Return a string representation of this tag for debugging.
+     * @return A string with the text of the title.
+     */
+    public String toString()
+    {
+        return "BLOCK BODY: " + getBlockBody();
+    }
+}
