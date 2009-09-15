6361675bc1db32eadd003f80547ddffe7a67758c
lendo.du <lendo.du@06ebe554-1442-0410-b41d-f121fd812e7c>
2009-09-15 08:22:40 +0000

    初始导入。

---
+++ ClassLoader/src/com/waioo/classloader/loadorder/A.java
@@ -0,0 +1,7 @@
+package com.waioo.classloader.loadorder;
+
+public class A {
+    public void print(){
+    	System.out.println("----------------from A.");
+    }
+}
---
+++ ClassLoader/src/com/waioo/classloader/loadorder/Main.java
@@ -0,0 +1,22 @@
+package com.waioo.classloader.loadorder;
+
+/**
+ * 此类主要是为了验证ClassLoader加载类的顺序
+ * 在执行时需要设置"jvm -verbose:class"
+ * 然后查看类的加载顺序，程序执行顺序以及哪些类会被加载
+ * 
+ * version 1.0 by Dan Du
+ * 2009-9-15 下午04:11:17
+ * @author Dan Du
+ */
+public class Main {
+	public static void main(String[] args) {
+		A a = new A();
+		a.print();
+		
+//		B b = new B();
+//		b.print();
+		
+        B b = null; //如果将上面两段代码删除，执行此段代码，注意类B是否会被加载
+	}
+}
